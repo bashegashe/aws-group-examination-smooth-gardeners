17972de1b6bf2e86cdc2adfab8bc4c339cddbe2c
Bachir Sbinati <sbinatibachir@gmail.com>
2023-09-12 14:58:09 +0200

    Add lambda function for deleting a booking

--- src/functions/deleteBooking.js
+++ src/functions/deleteBooking.js
@@ -3,30 +3,28 @@ import { sendResponse } from '../responses/index.js'
 import dayjs from 'dayjs'
 
 export const handler = async (event) => {
-  console.log('event ', event)
   try {
     const params = {
       TableName: process.env.TABLE_NAME,
       Key: {
-        PK: event.pathParameters.id
+        PK: `BOOKING#${event.pathParameters.id}`,
+        SK: 'BOOKINGS#META'
       }
     };
-
-    console.log(params)
 
     const { Item: booking } = await db.get(params).promise();
 
     if (!booking) {
-      throw new Error('Booking not found')
+      throw new Error('Booking not found');
     }
 
-    const daysBefore = dayjs().subtract(2, 'd')
+    const daysBefore = dayjs(booking.startDate).subtract(1, 'd');
 
-    if (!dayjs(booking.startDate).isBefore(daysBefore)) {
-      throw new Error('Booking cannot be cancelled less than 2 days before arrival')
+    if (!dayjs().isBefore(daysBefore)) {
+      throw new Error('Booking cannot be cancelled less than 2 days before arrival'); s
     }
 
-    // await db.delete(params).promise();
+    await db.delete(params).promise();
 
     return sendResponse(200, {
       success: true,
@@ -34,10 +32,6 @@ export const handler = async (event) => {
     })
   } catch (error) {
     let message = error?.details?.message || error?.message || 'Something went wrong'
-
-    if (error.code === 'ConditionalCheckFailedException') {
-      message = 'This booking cannot be canceled at this time.';
-    }
 
     return sendResponse(400, { error: message })
   }
