b045bc79e3f0de97c30bec78043e3bd38f2e96af
Bachir Sbinati <sbinatibachir@gmail.com>
2023-09-12 11:14:18 +0200

    Create function for getting all bookings

--- src/functions/getBookings.js
+++ src/functions/getBookings.js
@@ -1,5 +1,16 @@
 import { db } from '../services/db.js'
+import { sendResponse } from '../responses/index.js'
+import { createBookingResponse } from '../services/bookings.js'
 
 export const handler = async (event) => {
-  // TO BE IMPLEMENTED
+  const params = {
+    TableName: process.env.TABLE_NAME
+  }
+
+  const { Items: bookings } = await db.scan(params).promise()
+
+  return sendResponse(200, {
+    success: true,
+    bookings: bookings.map(booking => createBookingResponse(booking))
+  })
 }
